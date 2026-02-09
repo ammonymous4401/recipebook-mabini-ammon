ea755aa0965a9fe839ba1845b25dfe1f1b987cd8
Ammon Mabini <ramon.jr.mabini@student.ateneo.edu>
2026-02-09 21:59:26 +0800

    first commit

---
+++ lab1/recipebook/ledger/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import index, recipe_detail, recipe_list
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('list/', recipe_list, name='recipe_list'),
+    path('<int:recipe_id>/', recipe_detail, name='recipe_detail'),
+    ]
+
+# This might be needed, depending on your Django version
+app_name = 'ledger'
---
+++ lab1/recipebook/ledger/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello World! This came from the index view')
+
+
+recipes = [
+    {
+        "name": "Recipe 1",
+        "ingredients": [
+            {
+                "name": "tomato",
+                "quantity": "3pcs"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "pork",
+                "quantity": "1kg"
+            },
+            {
+                "name": "water",
+                "quantity": "1L"
+            },
+            {
+                "name": "sinigang mix",
+                "quantity": "1 packet"
+            }
+        ],
+        "link": "/recipe/1"
+    },
+    {
+        "name": "Recipe 2",
+        "ingredients": [
+            {
+                "name": "garlic",
+                "quantity": "1 head"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "vinegar",
+                "quantity": "1/2cup"
+            },
+            {
+                "name": "water",
+                "quantity": "1 cup"
+            },
+            {
+                "name": "salt",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "whole black peppers",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "pork",
+                "quantity": "1 kilo"
+            }
+        ],
+        "link": "/recipe/2"
+    }
+]
+
+
+def recipe_list(request):
+    ctx = {
+        "recipes": recipes,
+    }   
+    return render(request, 'ledger/recipe_list.html', ctx)
+
+
+def recipe_detail(request, recipe_id):
+    recipe = next((item for item in recipes 
+                   if item["link"] == f"/recipe/{recipe_id}"), None)
+    
+    return render(request, 'ledger/recipe_detail.html', {"recipe": recipe})
